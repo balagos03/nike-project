f8929192b73b18e0874ef7c5cd4a272ba07b8519
yasssssinnnn <yassinosss445@gmail.com>
2025-10-28 22:25:48 +0100

    hehe

--- pp.js
+++ pp.js
@@ -1,7 +1,8 @@
 const pctr= document.getElementById("tswirtboot")
 const headerpctr= document.getElementById("nike-seeklogo")
 const headerbar= document.getElementById("options")
-
+const input=document.getElementById("input1")
+const search=document.getElementById("search")
 
 function ejbdtswira() {
     pctr.classList.add("after")
@@ -11,3 +12,39 @@ function ejbdtswira() {
 }
 
 setTimeout(ejbdtswira,2000)
+
+
+function scaleup(){
+    input.style.transform= "scale(1.1)"
+    search.style.transform="scale(1.1)"
+}
+
+function scaledown(){
+    input.style.transform= "scale(1)"
+    search.style.transform="scale(1)"
+}
+
+
+
+input.addEventListener('mouseenter',scaleup)
+search.addEventListener('mouseenter',scaleup)
+input.addEventListener('mouseleave',scaledown)
+search.addEventListener('mouseleave',scaledown)
+
+const menubutton= document.getElementById("menu")
+const menu=document.getElementById("menupaper")
+const closemenu= document.getElementById("closemenu")
+menubutton.onclick= function () {
+    menu.classList.add("show")
+    closemenu.classList.add("show")
+    
+}
+
+closemenu.onclick= function(){
+    menu.classList.remove("show")
+    closemenu.classList.remove("show")
+
+}
+
+
+
